a7a43fe83494709d3ea4ce86298b61163d33a9cf
irongru <jcbfunc@gmail.com>
2023-02-08 08:11:34 -0600

    Initial Commit

---
+++ EmployeeDirectory/EmployeeDirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeDirectoryApp.swift
+//  EmployeeDirectory
+//
+//  Created by hope on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct EmployeeDirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
